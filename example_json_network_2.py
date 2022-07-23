9bed5a2b094312063421ea3711769ae6b5c8a387
Christian Siegl <christian.siegl@gmail.com>
2022-07-23 07:54:10 +0200

    add integration test for network 2

---
+++ example_json_network_2.py
@@ -0,0 +1,9 @@
+from Network import load_network_from_json
+from AdvancedNodalAnalysis import nodal_analysis_solver
+
+if __name__ == '__main__':
+    network = load_network_from_json('./example_network_2.json')
+    solution = nodal_analysis_solver(network)
+    for branch in network.branches:
+        print(f'{branch.node1}->{branch.node2} U={solution.get_voltage(branch):2.2f}V')
+        print(f'{branch.node1}->{branch.node2} I={solution.get_current(branch):2.2f}A')
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -29,9 +29,17 @@ def test_network_1_with_advanced_nodal_analysis() -> None:
 def test_network_2_with_classic_nodal_analysis() -> None:
     network = load_network_from_json('./example_network_2.json')
     with pytest.raises(ValueError):
-        solution = classic_nodal_analysis_solver(network)
+        classic_nodal_analysis_solver(network)
 
 def test_network_2_with_advanced_nodal_analysis() -> None:
     network = load_network_from_json('./example_network_2.json')
     solution = advanced_nodal_analysis_solver(network)
-    assert False
+    R1, R2, R3, U1 = tuple(network.branches)
+    np.testing.assert_almost_equal(solution.get_voltage(R1), 1.00, decimal=2)
+    np.testing.assert_almost_equal(solution.get_voltage(R2), 0.40, decimal=2)
+    np.testing.assert_almost_equal(solution.get_voltage(R3), 0.60, decimal=2)
+    np.testing.assert_almost_equal(solution.get_voltage(U1), 1.00, decimal=2)
+    np.testing.assert_almost_equal(solution.get_current(R1), 0.10, decimal=2)
+    np.testing.assert_almost_equal(solution.get_current(R2), 0.02, decimal=2)
+    np.testing.assert_almost_equal(solution.get_current(R3), 0.02, decimal=2)
+    np.testing.assert_almost_equal(solution.get_current(U1), 0.12, decimal=2)
