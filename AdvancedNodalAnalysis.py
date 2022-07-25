6c72cf86f8eb430f3cdcdedfdc3b27ee7c3a7cbe
Christian Siegl <christian.siegl@gmail.com>
2022-07-25 08:36:25 +0200

    refactor supernodes helper function

--- AdvancedNodalAnalysis.py
+++ AdvancedNodalAnalysis.py
@@ -1,63 +1,70 @@
 import ClassicNodalAnalysis as cna
 import numpy as np
-from Network import Network, Branch
-from typing import List
+from Network import Network, Branch, voltage_source
+from typing import List, Dict
 
 class AmbiguousElectricalPotential(Exception): pass
 
 def get_ideal_voltage_sources(network: Network) -> Network:
     return Network([b for b in network.branches if b.element.active and b.element.Z==0 and np.isfinite(b.element.U)])
 
-def get_supernodes(network: Network) -> List[int]:
+def get_supernodes(network: Network) -> Dict[int, Branch]:
     voltage_sources = get_ideal_voltage_sources(network)
     node_zero_voltage_sources = voltage_sources.branches_connected_to(node=0)
-    supernodes = [b.node1 if b.node1 != 0 else b.node2 for b in node_zero_voltage_sources]
-    if len(supernodes) != len(set(supernodes)): raise AmbiguousElectricalPotential
+    voltage_source_nodes = [tuple(sorted([b.node1, b.node2])) for b in node_zero_voltage_sources]
+    if len(voltage_source_nodes) != len(set(voltage_source_nodes)):
+        raise AmbiguousElectricalPotential
+    supernodes = {b.node1 if b.node1 != 0 else b.node2 : b for b in node_zero_voltage_sources}
     voltage_sources = Network([vs for vs in voltage_sources.branches if vs not in node_zero_voltage_sources])
     for vs in voltage_sources.branches:
-        if vs.node1 in supernodes and vs.node2 in supernodes: raise AmbiguousElectricalPotential
-        if vs.node1 not in supernodes:
-            supernodes.append(vs.node1)
+        if vs.node1 in supernodes.keys() and vs.node2 in supernodes.keys():
+            raise AmbiguousElectricalPotential
+        if vs.node1 not in supernodes.keys():
+            supernodes.update({vs.node1: vs})
         else:
-            supernodes.append(vs.node2)
+            supernodes.update({vs.node2: vs})
     return supernodes
 
 def create_node_matrix_from_network(network: Network) -> np.ndarray:
     Y = cna.create_node_admittance_matrix_from_network(network)
-    voltage_source_branches = [b for b in network.branch_list if b.element.active and b.element.Z == 0]
-    for vs in voltage_source_branches:
-        substituted_node = vs.node1 if vs.node1 != 0 else vs.node2
-        n_cols = Y.shape[0]
-        Y[:,substituted_node-1] = np.append(-1, np.zeros(n_cols-1))
+    for sn, b in get_supernodes(network).items():
+        Y[:,sn-1] = 0
+        if b.node1 > 0:
+            Y[b.node1-1, sn-1] = -1
+        if b.node2 > 0:
+            Y[b.node2-1, sn-1] = +1
     return Y
 
 def create_current_vector_from_network(network: Network) -> np.ndarray:
     I = cna.create_current_vector_from_network(network)
-    voltage_source_branches = [b for b in network.branch_list if b.element.active and b.element.Z == 0]
-    for vs in voltage_source_branches:
-        substituted_node = vs.node1 if vs.node1 != 0 else vs.node2
-        Y_all = sum([b.element.Y for b in network.branches_connected_to(substituted_node) if np.isfinite(b.element.Y)])
-        I[substituted_node-1] = -vs.element.U*Y_all
-        I[1] = vs.element.U*network.branches[1].element.Y
+    voltage_sources = get_ideal_voltage_sources(network)
+    for sn in get_supernodes(network):
+        I[sn-1] = 0
+        for vs in voltage_sources.branches_connected_to(sn):
+            Y_all = sum([b.element.Y for b in network.branches_connected_to(sn) if np.isfinite(b.element.Y)])
+            I[sn-1] += vs.element.U*Y_all
+    I[1] = vs.element.U*network.branches[1].element.Y
     return I
 
-def calculate_node_voltages_from_network(network: Network) -> np.ndarray:
-    Y = create_node_matrix_from_network(network)
-    I = create_current_vector_from_network(network)
-    return cna.calculate_node_voltages(Y, I)
-
 class NodalAnalysisSolution:
-    def __init__(self, node_voltages = np.ndarray) -> None:
-        self.node_voltages = node_voltages
+    def __init__(self, network : Network) -> None:
+        Y = create_node_matrix_from_network(network)
+        I = create_current_vector_from_network(network)
+        self.solution_vector = cna.calculate_node_voltages(Y, I)
+        self.super_nodes = get_supernodes(network)
+        self.super_node_voltages = 0###
+        self.node_potentials = self.solution_vector
+        for i in self.super_nodes:
+            self.node_potentials[i-1] = 0
     
     def get_voltage(self, branch: Branch) -> float:
-        return calculate_branch_voltage(self.node_voltages, branch.node1, branch.node2)
+        return 0
 
     def get_current(self, branch: Branch) -> float:
         return self.get_voltage(branch)/branch.element.Z.real
 
 def nodal_analysis_solver(network):
-    return NodalAnalysisSolution(calculate_node_voltages_from_network(network))
+    return NodalAnalysisSolution(network)
     
 
 if __name__ == '__main__':
@@ -68,8 +75,5 @@ if __name__ == '__main__':
     print(Y)
     I = create_current_vector_from_network(network)
     print(I)
-    V = calculate_node_voltages_from_network(network)
-    print(V)
-    vs = get_ideal_voltage_sources(network)
-    print(vs)
-    print(get_supernodes(network))
+    # V = calculate_node_voltages_from_network(network)
+    # print(V)
--- tests/NodalAnalysis/AdvancedNodalAnalysis/test_get_supernodes.py
+++ tests/NodalAnalysis/AdvancedNodalAnalysis/test_get_supernodes.py
@@ -28,12 +28,15 @@ def test_ambiguous_electrical_potential_leads_to_error() -> None:
         get_supernodes(network)
     
 def test_get_supernodes_identifies_all_supernodes() -> None:
+    vs1 = voltage_source(1)
+    vs2 = voltage_source(2)
     network = Network([
-        Branch(0, 1, voltage_source(1)),
+        Branch(0, 1, vs1),
         Branch(1, 2, resistor(2)),
-        Branch(2, 3, voltage_source(2)),
+        Branch(2, 3, vs2),
         Branch(3, 0, resistor(2)),
     ])
     supernodes = get_supernodes(network)
-    assert sorted(supernodes) == [1, 2]
+    assert supernodes[1].element == vs1
+    assert supernodes[2].element == vs2
     
