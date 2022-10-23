4491c8e919b75aab25d13385d355579af5bd2acb
Christian Siegl <christian.siegl@gmail.com>
2022-10-23 17:55:58 +0200

    add new tests pointing out bug

--- tests/NodalAnalysis/AdvancedNodalAnalysis/test_create_node_matrix_from_network.py
+++ tests/NodalAnalysis/AdvancedNodalAnalysis/test_create_node_matrix_from_network.py
@@ -1,6 +1,25 @@
 from CircuitCalculator.AdvancedNodalAnalysis import create_node_matrix_from_network
-from CircuitCalculator.Network import Network, Branch, voltage_source, resistor, current_source
+from CircuitCalculator.Network import Network, Branch, voltage_source, resistor, current_source, conductor
 import numpy as np
+
+def test_ideal_voltage_sources_are_ignored_but_matrix_is_finite() -> None:
+    network = Network([
+        Branch(0, 2, voltage_source(1)),
+        Branch(2, 1, voltage_source(1)),
+        Branch(1, 0, conductor(1))
+    ])
+    Y = create_node_matrix_from_network(network)
+    assert np.all(np.isfinite(Y))
+
+def test_ideal_voltage_sources_contribute_to_dimension_of_matrix() -> None:
+    network = Network([
+        Branch(0, 2, voltage_source(1)),
+        Branch(2, 1, voltage_source(1)),
+        Branch(1, 0, conductor(1))
+    ])
+    Y = create_node_matrix_from_network(network)
+
+    assert Y.shape == (network.number_of_nodes-1, network.number_of_nodes-1)
 
 def test_create_node_matrix_from_reference_network_3() -> None:
     R1, R2, R3, R4, R5 = 10, 20, 30, 40, 50
--- tests/NodalAnalysis/ClassicNodalAnalysis/test_create_node_admittances_from_network.py
+++ tests/NodalAnalysis/ClassicNodalAnalysis/test_create_node_admittances_from_network.py
@@ -1,5 +1,5 @@
 from CircuitCalculator.ClassicNodalAnalysis import create_node_admittance_matrix_from_network
-from CircuitCalculator.Network import Network, Branch, conductor
+from CircuitCalculator.Network import Network, Branch, conductor, voltage_source
 import numpy as np
 
 def test_node_admittance_matrix_is_correct() -> None:
@@ -30,5 +30,22 @@ def test_node_admittance_matrix_sorts_node_indices() -> None:
 
     np.testing.assert_almost_equal(Y, Y_ref)
 
+def test_ideal_voltage_sources_are_ignored_but_matrix_is_finite() -> None:
+    network = Network([
+        Branch(0, 2, voltage_source(1)),
+        Branch(2, 1, voltage_source(1)),
+        Branch(1, 0, conductor(1))
+    ])
+    Y = create_node_admittance_matrix_from_network(network)
+    assert np.all(np.isfinite(Y))
 
+def test_ideal_voltage_sources_contribute_to_dimension_of_matrix() -> None:
+    network = Network([
+        Branch(0, 2, voltage_source(1)),
+        Branch(2, 1, voltage_source(1)),
+        Branch(1, 0, conductor(1))
+    ])
+    Y = create_node_admittance_matrix_from_network(network)
+
+    assert Y.shape == (network.number_of_nodes-1, network.number_of_nodes-1)
     
