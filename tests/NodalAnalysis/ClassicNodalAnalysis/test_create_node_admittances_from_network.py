761b642a2d0d89d46ffcb8b5c51cec39c508bef0
Christian Siegl <christian.siegl@gmail.com>
2022-10-31 09:40:49 +0100

    ideal voltage sources are not allowed in classic mode

--- tests/NodalAnalysis/ClassicNodalAnalysis/test_create_node_admittances_from_network.py
+++ tests/NodalAnalysis/ClassicNodalAnalysis/test_create_node_admittances_from_network.py
@@ -29,23 +29,3 @@ def test_node_admittance_matrix_sorts_node_indices() -> None:
     Y = create_node_admittance_matrix_from_network(network)
 
     np.testing.assert_almost_equal(Y, Y_ref)
-
-def test_ideal_voltage_sources_are_ignored_but_matrix_is_finite() -> None:
-    network = Network([
-        Branch(0, 2, voltage_source(1)),
-        Branch(2, 1, voltage_source(1)),
-        Branch(1, 0, conductor(1))
-    ])
-    Y = create_node_admittance_matrix_from_network(network)
-    assert np.all(np.isfinite(Y))
-
-def test_ideal_voltage_sources_contribute_to_dimension_of_matrix() -> None:
-    network = Network([
-        Branch(0, 2, voltage_source(1)),
-        Branch(2, 1, voltage_source(1)),
-        Branch(1, 0, conductor(1))
-    ])
-    Y = create_node_admittance_matrix_from_network(network)
-
-    assert Y.shape == (network.number_of_nodes-1, network.number_of_nodes-1)
-    
