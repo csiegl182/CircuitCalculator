39c5f9e77a2ed4d276ed22e54061091627c1a05d
Christian Siegl <christian.siegl@gmail.com>
2023-03-29 22:08:27 +0200

    update examples

--- examples/networks/simple_circuit/example_network_6_schemdraw.py
+++ examples/networks/simple_circuit/example_network_6_schemdraw.py
@@ -1,29 +1,29 @@
 from CircuitCalculator.SimpleCircuit.Elements import Schematic, Resistor, Line, LabelNode
-from CircuitCalculator.SimpleCircuit.DiagramParser import SchematicDiagramAnalyzer, network_parser
+from CircuitCalculator.SimpleCircuit.DiagramParser import network_parser
 from CircuitCalculator.EquivalentSources import calculate_total_impedeance
 
 if __name__ == '__main__':
-    with Schematic() as schem:
-        schem += LabelNode(id='1', id_loc='S')
-        schem += Line().up().length(4)
-        schem += Resistor(R=1, name='R1').right()
-        schem += Line().up().length(2)
-        schem += (R4:=Resistor(R=4, name='R4').right())
-        schem += Resistor(R=5, name='R5').right()
-        schem += Line().down().length(2)
-        schem += Line().down().length(2)
-        schem += (R3:=Resistor(R=3, name='R3').left().length(6))
-        schem += Line().up().length(2)
-        schem += Line().at(R3.start).down().length(2)
-        schem += Line().left().length(4.5)
-        schem += Resistor(R=2, name='R2').left()
-        schem += Line().left().length(1.5)
-        schem += Line().at(R4.end).up().length(2)
-        schem += Line().right().length(1.5)
-        schem += Resistor(R=6, name='R6').right()
-        schem += Line().right().length(1.5)
-        schem += Line().down().length(4)
-        schem += LabelNode(id='2', id_loc='S')
-        schem += Resistor(R=7, name='R7').left()
-        R = calculate_total_impedeance(network_parser(schem), '1', '2').real
+    with Schematic(unit=3) as schematic:
+        schematic += LabelNode(id='1', id_loc='S')
+        schematic += Line().up().length(4)
+        schematic += Resistor(R=1, name='R1').right()
+        schematic += Line().up().length(2)
+        schematic += (R4:=Resistor(R=4, name='R4').right())
+        schematic += Resistor(R=5, name='R5').right()
+        schematic += Line().down().length(2)
+        schematic += Line().down().length(2)
+        schematic += (R3:=Resistor(R=3, name='R3').left().length(6))
+        schematic += Line().up().length(2)
+        schematic += Line().at(R3.start).down().length(2)
+        schematic += Line().left().length(4.5)
+        schematic += Resistor(R=2, name='R2').left()
+        schematic += Line().left().length(1.5)
+        schematic += Line().at(R4.end).up().length(2)
+        schematic += Line().right().length(1.5)
+        schematic += Resistor(R=6, name='R6').right()
+        schematic += Line().right().length(1.5)
+        schematic += Line().down().length(4)
+        schematic += LabelNode(id='2', id_loc='S')
+        schematic += Resistor(R=7, name='R7').left()
+        R = calculate_total_impedeance(network_parser(schematic), '1', '2').real
         print(f'{R=:4.2f}Ohm')
--- examples/networks/simple_circuit/example_network_7_schemdraw.py
+++ examples/networks/simple_circuit/example_network_7_schemdraw.py
@@ -1,19 +1,19 @@
 from CircuitCalculator.SimpleCircuit.Elements import Schematic, VoltageSource, Resistor, Line, LabelNode
-from CircuitCalculator.SimpleCircuit.DiagramParser import SchematicDiagramAnalyzer, SchematicDiagramSolution
+from CircuitCalculator.SimpleCircuit.DiagramParser import SchematicDiagramAnalyzer, SchematicDiagramSolution, network_parser
 from CircuitCalculator.NodalAnalysis import nodal_analysis_solver
 
 R1, R2, R3, R4 = 10, 20, 30, 40
 Uq1, Uq2, Uq3, Iq4 = 1, 2, 3, 0.1
 
-with Schematic(unit=5) as d:
-    d += VoltageSource(V=Uq1, name='Uq1', precision=1).up()
-    d += LabelNode(id='1', id_loc='W')
-    d += VoltageSource(V=Uq2, name='Uq2', precision=1).up()
-    d += (_R1:=Resistor(R=R1, name='R1').right())
-    d += Line().down()
-    d += Line().down()
-    d += Line().left()
-    schemdraw_network = SchematicDiagramAnalyzer(d)
-    schemdraw_solution = SchematicDiagramSolution(schemdraw_network, nodal_analysis_solver)
-    d += schemdraw_solution.draw_voltage('R1')
-    d += schemdraw_solution.draw_current('R1', ofst=1.2)
+with Schematic(unit=5) as schematic:
+    schematic += VoltageSource(V=Uq1, name='Uq1', precision=1).up()
+    schematic += LabelNode(id='1', id_loc='W')
+    schematic += VoltageSource(V=Uq2, name='Uq2', precision=1).up()
+    schematic += (_R1:=Resistor(R=R1, name='R1').right())
+    schematic += Line().down()
+    schematic += Line().down()
+    schematic += Line().left()
+    solution = nodal_analysis_solver(network_parser(schematic))
+    schemdraw_solution = SchematicDiagramSolution(SchematicDiagramAnalyzer(schematic), solution)
+    schematic += schemdraw_solution.draw_voltage('R1')
+    schematic += schemdraw_solution.draw_current('R1')
