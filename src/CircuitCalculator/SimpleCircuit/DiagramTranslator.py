09698ce4f8e3e286617742453c0fa5566aab3180
Christian Siegl <christian.siegl@gmail.com>
2023-05-01 17:55:00 +0200

    remove unused methods

--- src/CircuitCalculator/SimpleCircuit/DiagramTranslator.py
+++ src/CircuitCalculator/SimpleCircuit/DiagramTranslator.py
@@ -11,12 +11,3 @@ from typing import List
 def circuit_translator(schematic: elm.Schematic) -> Circuit:
     schematic_diagram = SchematicDiagramParser(schematic)
     return schematic_diagram.translate_elements(circuit_translator_map)
-
-def network_translator(schematic: elm.Schematic, w: List[float] = []) -> List[Network]:
-    circuit = circuit_translator(schematic)
-    if len(w) == 0:
-        w = circuit.w
-    return transform(circuit=circuit, w=w)
-
-def dc_network_translator(schematic: elm.Schematic) -> Network:
-    return network_translator(schematic, w=[0])[0]
