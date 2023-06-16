ac672968ef3412859c6c29307d2e680fb62b7de2
Christian Siegl <christian.siegl@gmail.com>
2023-06-16 17:07:19 +0200

    create integration test for example 2

--- examples/networks/json/example_network_2.py
+++ examples/networks/json/example_network_2.py
@@ -4,6 +4,7 @@ from CircuitCalculator.Network.NodalAnalysis import nodal_analysis_solver as sol
 if __name__ == '__main__':
     network = load_network_from_json('./examples/networks/json/example_network_2.json')
     solution = solver(network)
-    print(f'I={solution.get_current("Iq"):2.2f}A')
-    print(f'I={solution.get_current("R"):2.2f}A')
-    print(f'V={solution.get_voltage("R"):2.2f}V')
+    for id in network.branch_ids:
+        print(f'I({id})={solution.get_current(id):2.2f}A')
+    for id in network.branch_ids:
+        print(f'V({id})={solution.get_voltage(id):2.2f}V')
