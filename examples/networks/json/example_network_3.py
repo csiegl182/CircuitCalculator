a6c8a890ec56ca8cc35251334bdb77a94c5ac131
Christian Siegl <christian.siegl@gmail.com>
2023-06-16 17:09:45 +0200

    update look and feel of examples 1-3

--- examples/networks/json/example_network_3.py
+++ examples/networks/json/example_network_3.py
@@ -4,9 +4,7 @@ from CircuitCalculator.Network.NodalAnalysis import nodal_analysis_solver
 if __name__ == '__main__':
     network = load_network_from_json('./examples/networks/json/example_network_3.json')
     solution = nodal_analysis_solver(network)
-    print(f'U(I1)={solution.get_voltage("I1"):2.2f}V')
-    print(f'U(R1)={solution.get_voltage("R1"):2.2f}V')
-    print(f'U(R2)={solution.get_voltage("R2"):2.2f}V')
-    print(f'I(I1)={solution.get_current("I1"):2.2f}A')
-    print(f'I(R1)={solution.get_current("R1"):2.2f}A')
-    print(f'I(R2)={solution.get_current("R2"):2.2f}A')
+    for id in network.branch_ids:
+        print(f'I({id})={solution.get_current(id):2.2f}A')
+    for id in network.branch_ids:
+        print(f'V({id})={solution.get_voltage(id):2.2f}V')
