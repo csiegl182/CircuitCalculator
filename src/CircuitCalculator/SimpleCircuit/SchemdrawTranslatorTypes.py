9b556393adceaba8d8ca4fd52f7ac9b7f75259c0
Christian Siegl <christian.siegl@gmail.com>
2023-11-29 12:30:57 +0100

    fix typing issues

--- src/CircuitCalculator/SimpleCircuit/SchemdrawTranslatorTypes.py
+++ src/CircuitCalculator/SimpleCircuit/SchemdrawTranslatorTypes.py
@@ -3,6 +3,6 @@ import schemdraw.elements
 from ..Circuit.components import Component
 
 SchemdrawElement = TypeVar('SchemdrawElement', bound=schemdraw.elements.Element)
-SchemdrawElementTranslator = Callable[[SchemdrawElement, tuple[str]], Component | None] | Callable[[SchemdrawElement, tuple[str, str]], Component | None]
+SchemdrawElementTranslator = Callable[[SchemdrawElement, tuple[str, ...]], Component | None] | Callable[[SchemdrawElement, tuple[str, ...]], Component | None]
 
 ElementTranslatorMap = dict[Type[schemdraw.elements.Element], SchemdrawElementTranslator]
