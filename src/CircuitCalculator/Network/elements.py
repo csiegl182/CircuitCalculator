63c13e991ae72f8233f987fb969c565e613ef05a
Christian Siegl <christian.siegl@gmail.com>
2024-04-07 02:54:32 +0200

    add load elements for networks

--- src/CircuitCalculator/Network/elements.py
+++ src/CircuitCalculator/Network/elements.py
@@ -82,6 +82,19 @@ def resistor(name : str, R : float) -> NortenTheveninElement:
 def conductor(name : str, G : float) -> NortenTheveninElement:
     return TheveninElement(Y=G, I=0, name=name, type='conductor')
 
+def load(name : str, P : float, V_ref: float = -1, I_ref: float = -1, Q : float = 0) -> NortenTheveninElement:
+    if V_ref < 0 and I_ref < 0:
+        raise AttributeError('A reference voltage or reference current has to be defined for a load.')
+    if V_ref > 0 and I_ref > 0:
+        raise AttributeError('You can only define a reference voltage or a reference current for a load.')
+    if V_ref == 0 and I_ref < 0:
+        raise ValueError('Reference voltage must be greater than zero.')
+    if I_ref <= 0 and V_ref < 0:
+        raise ValueError('Reference current must be greater than zero.')
+    if V_ref > 0:
+        return TheveninElement(Y=complex(P, Q)/V_ref**2, I=0, name=name, type='load')
+    return NortenElement(Z=complex(P, Q)/I_ref**2, V=0, name=name, type='load')
+
 def voltage_source(name : str, V : complex, Z : complex = 0) -> NortenTheveninElement:
     return NortenElement(V=V, Z=Z, name=name, type='voltage_source')
 
--- tests/Network/elements/test_elements.py
+++ tests/Network/elements/test_elements.py
@@ -1,50 +1,86 @@
-from CircuitCalculator.Network.elements import current_source, resistor, conductor, voltage_source
+import pytest
+from hypothesis import given, strategies as st
+import CircuitCalculator.Network.elements as elm
 from numpy import inf, isnan
 
 def test_zero_resistance_has_inf_conductance() -> None:
-    R = resistor('R1', R=0)
+    R = elm.resistor('R1', R=0)
     assert R.Y == inf
 
 def test_inf_resistance_has_zero_conductance() -> None:
-    R = resistor('R1', R=inf)
+    R = elm.resistor('R1', R=inf)
     assert R.Y == 0
 
 def test_zero_conductance_has_inf_resistance() -> None:
-    G = conductor('G1', G=0)
+    G = elm.conductor('G1', G=0)
     assert G.Z == inf
 
 def test_inf_conductance_has_zero_resistance() -> None:
-    G = conductor('G1', G=inf)
+    G = elm.conductor('G1', G=inf)
     assert G.Z == 0
 
 def test_linear_current_source_with_zero_conductance_has_inf_resistance() -> None:
-    I = current_source('Is1', I=1, Y=0)
+    I = elm.current_source('Is1', I=1, Y=0)
     assert I.Z == inf
 
 def test_linear_current_source_with_inf_conductance_has_zero_resistance() -> None:
-    I = current_source('Is1', I=1, Y=inf)
+    I = elm.current_source('Is1', I=1, Y=inf)
     assert I.Z == 0
 
 def test_linear_current_source_with_inf_conductance_has_zero_voltage() -> None:
-    I = current_source('Is1', I=1, Y=inf)
+    I = elm.current_source('Is1', I=1, Y=inf)
     assert I.V == 0
 
 def test_linear_current_source_with_zero_conductance_has_nan_voltage() -> None:
-    I = current_source('Is1', I=1, Y=0)
+    I = elm.current_source('Is1', I=1, Y=0)
     assert isnan(I.V)
 
 def test_linear_voltage_source_with_zero_resistance_has_inf_conductance() -> None:
-    U = voltage_source('Us1', V=1, Z=0)
+    U = elm.voltage_source('Us1', V=1, Z=0)
     assert U.Y == inf
 
 def test_linear_voltage_source_with_inf_resistance_has_zero_conductance() -> None:
-    U = voltage_source('Us1', V=1, Z=inf)
+    U = elm.voltage_source('Us1', V=1, Z=inf)
     assert U.Y == 0
 
 def test_linear_voltage_source_with_inf_resistance_has_zero_current() -> None:
-    U = voltage_source('Us1', V=1, Z=inf)
+    U = elm.voltage_source('Us1', V=1, Z=inf)
     assert U.I == 0
 
 def test_linear_voltage_source_with_zero_resistance_has_nan_current() -> None:
-    U = voltage_source('Us1', V=1, Z=0)
+    U = elm.voltage_source('Us1', V=1, Z=0)
     assert isnan(U.I)
+
+def test_load_raises_error_when_not_providing_reference_values() -> None:
+    with pytest.raises(AttributeError):
+        elm.load(name='load', P = 1)
+
+def test_load_raises_error_when_not_providing_both_reference_values() -> None:
+    with pytest.raises(AttributeError):
+        elm.load(name='load', P = 1, V_ref = 1, I_ref = 1)
+
+def test_load_raises_error_when_reference_voltage_is_zero() -> None:
+    with pytest.raises(ValueError):
+        elm.load(name='load', P = 1, V_ref = 0)
+
+def test_load_raises_error_when_reference_current_is_zero() -> None:
+    with pytest.raises(ValueError):
+        elm.load(name='load', P = 1, I_ref = 0)
+
+def test_load_has_zero_voltage() -> None:
+    L = elm.load(name='load', P = 1, V_ref = 1)
+    assert L.V == 0
+
+def test_load_has_zero_current() -> None:
+    L = elm.load(name='load', P = 1, V_ref = 1)
+    assert L.I == 0
+
+@given(st.floats(min_value=0, max_value=1e3, allow_subnormal=False), st.floats(min_value=0, max_value=1e3, allow_subnormal=False), st.floats(min_value=1e-3, max_value=1e3, allow_subnormal=False))
+def test_load_resistance_is_calculated_correctly_from_reference_voltage(P, Q, V_ref) -> None:
+    L = elm.load(name='load', P = P, Q = Q, V_ref = V_ref)
+    assert L.Y == complex(P, Q)/V_ref**2
+
+@given(st.floats(min_value=0, max_value=1e3, allow_subnormal=False), st.floats(min_value=0, max_value=1e3, allow_subnormal=False), st.floats(min_value=1e-3, max_value=1e3, allow_subnormal=False))
+def test_load_resistance_is_calculated_correctly_from_reference_current(P, Q, I_ref) -> None:
+    L = elm.load(name='load', P = P, Q = Q, I_ref = I_ref)
+    assert L.Z == complex(P, Q)/I_ref**2
