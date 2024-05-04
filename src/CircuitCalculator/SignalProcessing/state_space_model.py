132aa894c34d5f8de97ca3f1cdbc6d4d65ddbf31
Christian Siegl <christian.siegl@gmail.com>
2024-05-04 14:03:59 +0200

    implement basic state space model data container

--- src/CircuitCalculator/SignalProcessing/state_space_model.py
+++ src/CircuitCalculator/SignalProcessing/state_space_model.py
@@ -1,5 +1,4 @@
 import numpy as np
-from typing import Protocol
 from dataclasses import dataclass
 
 @dataclass(frozen=True)
@@ -12,21 +11,23 @@ class StateSpaceModel:
     def __post_init__(self) -> None:
         if self.A.shape[0] != self.A.shape[1]:
             raise ValueError('Matrix A must be square')
-        # n_states = self.A.shape[0]
-        # n_inputs = self.B.shape[1]
-        # if self.B.shape[0] != self.A.shape[0]:
-        #     raise ValueError('Number of columns of matrix B must be equal to number of rows of matrix A')
-        # if self.C.shape[1] != self.A.shape[0]:
-        #     raise ValueError('Number of coluns of matrix B must be equal to number of rows of matrix A')
+        if self.B.shape[0] != self.A.shape[0]:
+            raise ValueError('Number of columns of matrix B must be equal to number of rows of matrix A')
+        if self.C.shape[1] != self.A.shape[0]:
+            raise ValueError('Number of columns of matrix B must be equal to number of rows of matrix A')
+        if self.D.shape[0] != self.C.shape[0]:
+            raise ValueError('Number of rows of matrix D must be equal to number of rows of matrix C')
+        if self.D.shape[1] != self.B.shape[1]:
+            raise ValueError('Number of columns of matrix D must be equal to number of columns of matrix B')
 
     @property
     def n_states(self) -> int:
         return self.A.shape[0]
 
     @property
-    def n_input(self) -> int:
+    def n_inputs(self) -> int:
         return self.B.shape[1]
 
     @property
-    def n_output(self) -> int:
+    def n_outputs(self) -> int:
         return self.C.shape[0]
