b571f6941b96f9480fb7e6dee95375893ddafb54
Christian Siegl <christian.siegl@gmail.com>
2024-05-31 22:06:21 +0200

    update state space matrices tests

--- tests/Network/NodalAnalysis/state_space_representation/test_output_matrix_generation.py
+++ tests/Network/NodalAnalysis/state_space_representation/test_output_matrix_generation.py
@@ -1,5 +1,5 @@
 from CircuitCalculator.Network.network import Network, Branch
-from CircuitCalculator.Network.elements import resistor, voltage_source, current_source, open_circuit
+from CircuitCalculator.Network.elements import resistor, voltage_source, current_source, open_circuit, short_circuit
 from CircuitCalculator.Network.NodalAnalysis.state_space_model import state_space_matrices
 import numpy as np
 from numpy.testing import assert_almost_equal
@@ -60,3 +60,61 @@ def test_output_matrix_of_transient_network_4() -> None:
     ])
     _, _, C, _ = state_space_matrices(network, {'C1' : C1, 'C2' : C2})
     assert_almost_equal(C, np.array([[0, 0], [1, 1], [0, 1], [1/R1, 1/R1]]))
+
+def test_output_matrix_of_transient_network_5() -> None:
+    V0 = 5
+    R1, R2, R3 = 10, 20, 30
+    L = 0.1
+    network = Network([
+        Branch('1', '0', voltage_source('Vq', V=V0)),
+        Branch('1', '2', resistor('R1', R=R1)),
+        Branch('2', '0', resistor('R2', R=R2)),
+        Branch('2', '3', resistor('R3', R=R3)),
+        Branch('3', '0', short_circuit('L'))
+    ])
+    _, _, C, _ = state_space_matrices(network, l_values={'L' : L})
+    assert_almost_equal(C, np.array([[0], [-R1*R2/(R1+R2)], [-(R1*R2+R1*R3+R2*R3)/(R1+R2)], [1], [-R2/(R1+R2)]]))
+
+def test_output_matrix_of_transient_network_6() -> None:
+    R1, R2, R3 = 10, 20, 30
+    L = 0.1
+    V0 = 5
+    network = Network([
+        Branch('1', '0', voltage_source('Vq', V=V0)),
+        Branch('1', '2', resistor('R1', R=R1)),
+        Branch('2', '0', resistor('R2', R=R2)),
+        Branch('2', '3', short_circuit('L')),
+        Branch('3', '0', resistor('R3', R=R3))
+    ])
+    _, _, C, _ = state_space_matrices(network, l_values={'L' : L})
+    assert_almost_equal(C, np.array([[0], [-R1*R2/(R1+R2)], [R3], [1], [-R2/(R1+R2)]]))
+
+def test_output_matrix_of_transient_network_7() -> None:
+    R1, R2, R3 = 10, 20, 30
+    L = 0.1
+    V0 = 5
+    I0 = 1
+    network = Network([
+        Branch('1', '0', voltage_source('Vq', V=V0)),
+        Branch('1', '2', resistor('R1', R=R1)),
+        Branch('2', '0', resistor('R2', R=R2)),
+        Branch('2', '3', short_circuit('L')),
+        Branch('3', '0', resistor('R3', R=R3)),
+        Branch('0', '3', current_source('Iq', I=I0))
+    ])
+    _, _, C, _ = state_space_matrices(network, l_values={'L' : L})
+    assert_almost_equal(C, np.array([[0], [-R1*R2/(R1+R2)], [R3], [1], [-R2/(R1+R2)]]))
+
+def test_output_matrix_of_transient_network_8() -> None:
+    R1, R2 = 10, 20
+    L1, L2 = 0.1, 0.2
+    V0 = 5
+    network = Network([
+        Branch('1', '0', voltage_source('Vq', V=V0)),
+        Branch('1', '2', resistor('R1', R=R1)),
+        Branch('2', '3', short_circuit('L1')),
+        Branch('3', '0', resistor('R2', R=R2)),
+        Branch('3', '0', short_circuit('L2'))
+    ])
+    _, _, C, _ = state_space_matrices(network, l_values={'L1' : L1, 'L2' : L2})
+    assert_almost_equal(C, np.array([[0, 0], [-R1, 0], [R2, -R2], [1, 0], [0, 1], [-1, 0]]))
