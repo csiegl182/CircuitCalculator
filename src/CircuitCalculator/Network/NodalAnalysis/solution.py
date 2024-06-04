2483d5e5d35c9fce89a334f16e298d43a9ee7a4b
Christian Siegl <christian.siegl@gmail.com>
2024-06-04 20:14:50 +0200

    remove unused supernodes class

--- src/CircuitCalculator/Network/NodalAnalysis/solution.py
+++ src/CircuitCalculator/Network/NodalAnalysis/solution.py
@@ -3,7 +3,6 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from ..network import Network
 from . import label_mapping as map
-from .supernodes import SuperNodes
 
 @dataclass
 class NodalAnalysisSolution(ABC):
@@ -23,10 +22,6 @@ class NodalAnalysisSolution(ABC):
     @property
     def _voltage_source_mapping(self) -> map.LabelMapping:
         return self.voltage_source_mapper(self.network)
-
-    @property
-    def _super_nodes(self) -> SuperNodes:
-        return SuperNodes(self.network)
 
     @abstractmethod
     def get_potential(self, node_id: str) -> Any:
--- src/CircuitCalculator/Network/transformers.py
+++ src/CircuitCalculator/Network/transformers.py
@@ -1,6 +1,5 @@
 from .network import Network, Branch
 from .elements import NortenTheveninElement, is_current_source, is_voltage_source, is_short_circuit, is_open_circuit, impedance, admittance, is_open_circuit, is_short_circuit
-from .NodalAnalysis.supernodes import SuperNodes
 
 def switch_ground_node(network: Network, new_ground: str) -> Network:
     return Network(network.branches, new_ground)
@@ -15,9 +14,8 @@ def remove_open_circuit_elements(network: Network) -> Network:
 
 def remove_short_circuit_elements(network: Network, keep: list[NortenTheveninElement] = []) -> Network:
     branches = network.branches
-    super_nodes = SuperNodes(network)
     short_circuits = [b for b in network.branches if is_short_circuit(b.element) and b.element not in keep]
-    short_circuit_nodes = [(vs.node1, vs.node2) if super_nodes.is_active(vs.node1) else (vs.node2, vs.node1) for vs in short_circuits]
+    short_circuit_nodes = [(vs.node1, vs.node2) if not network.is_zero_node(vs.node1) else (vs.node2, vs.node1) for vs in short_circuits]
     for an, rn in short_circuit_nodes:
         branches = [Branch(rn, b.node2, b.element) if b.node1 == an else b for b in branches]
         branches = [Branch(b.node1, rn, b.element) if b.node2 == an else b for b in branches]
