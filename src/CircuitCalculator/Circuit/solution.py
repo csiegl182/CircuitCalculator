fix issue of power calculation of complex networks

src/CircuitCalculator/Circuit/solution.py
@@ -53,22 +53,31 @@ class DCSolution(CircuitSolution):
 class ComplexSolution(CircuitSolution):
     solver: NetworkSolver = field(default=nodal_analysis_bias_point_solver)
     w: float = 0
+    peak_values: bool = False
 
     def __post_init__(self):
         network = transform(self.circuit, w=[self.w])[0]
         self._solution = self.solver(network)
 
     def get_voltage(self, component_id: str) -> complex:
-        return self._solution.get_voltage(component_id)
+        if self.peak_values:
+            return self._solution.get_voltage(component_id)
+        return self._solution.get_voltage(component_id)/np.sqrt(2)
 
     def get_current(self, component_id: str) -> complex:
-        return self._solution.get_current(component_id)
+        if self.peak_values:
+            return self._solution.get_current(component_id)
+        return self._solution.get_current(component_id)/np.sqrt(2)
 
     def get_potential(self, node_id: str) -> complex:
-        return self._solution.get_potential(node_id)
+        if self.peak_values:
+            return self._solution.get_potential(node_id)
+        return self._solution.get_potential(node_id)/np.sqrt(2)
 
     def get_power(self, component_id: str) -> complex:
-        return self._solution.get_voltage(component_id)*np.conj(self._solution.get_current(component_id))
+        if self.peak_values:
+            return 1/2*self.get_voltage(component_id)*np.conj(self.get_current(component_id))
+        return self.get_voltage(component_id)*np.conj(self.get_current(component_id))
 
 @dataclass
 class TimeDomainSolution(CircuitSolution):
@@ -102,11 +111,11 @@ class TimeDomainSolution(CircuitSolution):
 class FrequencyDomainSolution(CircuitSolution):
     w_max: float = field(default=0)
     solver: NetworkSolver = field(default=nodal_analysis_bias_point_solver)
+    peak_values: bool = False
 
     def __post_init__(self):
         self.w = frequency_components(self.circuit, self.w_max)
-        networks = transform(self.circuit, w=self.w)
-        self._solutions = [self.solver(network) for network in networks]
+        self._solutions = [ComplexSolution(circuit=self.circuit, solver=self.solver, w=w, peak_values=self.peak_values) for w in self.w]
 
     def get_voltage(self, component_id: str) -> FrequencyDomainSeries:
         voltages = np.array([solution.get_voltage(component_id) for solution in self._solutions])
@@ -121,10 +130,8 @@ class FrequencyDomainSolution(CircuitSolution):
         return np.array(self.w), potentials
 
     def get_power(self, component_id: str) -> FrequencyDomainSeries:
-        w, voltage = self.get_voltage(component_id)
-        _, current = self.get_current(component_id)
-        return w, voltage*np.conj(current)
-
+        power = np.array([solution.get_power(component_id) for solution in self._solutions])
+        return np.array(self.w), power
 
 @dataclass
 class TransientSolution(CircuitSolution):