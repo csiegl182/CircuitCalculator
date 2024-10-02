cca4f75cc7ddb90351da3765fa65da1663ebdf1b
Christian Siegl <christian.siegl@gmail.com>
2024-10-02 07:57:33 +0200

    fix test issue

--- tests/SignalProcessing/periodic_functions/test_harmonic_coefficients.py
+++ tests/SignalProcessing/periodic_functions/test_harmonic_coefficients.py
@@ -18,24 +18,24 @@ def test_phase_method_raises_error_when_passing_negative_index(A, phi, index) ->
             harmonic_implementation.phase(index)
 
 @given(st.floats(min_value=0, allow_subnormal=False), st.floats(min_value=0, max_value=2*np.pi, allow_subnormal=False))
-def test_real_part_is_correct(A, phi) -> None:
+def test_a_coefficient_is_correct(A, phi) -> None:
     class HarmonicImplementation(AbstractHarmonicCoefficients):
         def _amplitude_coefficient(self, _):
             return self.amplitude0
         def _phase_coefficient(self, _):
             return self.phase0
     hi = HarmonicImplementation(A, phi)
-    assert hi.real(0) == A*np.cos(phi)
+    assert hi.a(0) == A*np.cos(phi)
 
 @given(st.floats(min_value=0, allow_subnormal=False, allow_infinity=False), st.floats(min_value=0, max_value=2*np.pi, allow_subnormal=False))
-def test_imag_part_is_correct(A, phi) -> None:
+def test_b_coefficient_is_correct(A, phi) -> None:
     class HarmonicImplementation(AbstractHarmonicCoefficients):
         def _amplitude_coefficient(self, _):
             return self.amplitude0
         def _phase_coefficient(self, _):
             return self.phase0
     hi = HarmonicImplementation(A, phi)
-    assert hi.imag(0) == A*np.sin(phi)
+    assert hi.b(0) == A*np.sin(phi)
 
 @given(st.floats(min_value=0, allow_subnormal=False, allow_infinity=False), st.floats(min_value=0, max_value=2*np.pi, allow_subnormal=False))
 def test_complex_part_is_correct(A, phi) -> None:
