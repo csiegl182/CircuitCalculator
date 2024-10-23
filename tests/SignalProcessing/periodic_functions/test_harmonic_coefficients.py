75284b319f03a7c73479d323199f6cf23ce0e45b
Christian Siegl <christian.siegl@gmail.com>
2024-10-23 20:13:53 +0200

    align fourier coefficient definition to complex analysis

--- tests/SignalProcessing/periodic_functions/test_harmonic_coefficients.py
+++ tests/SignalProcessing/periodic_functions/test_harmonic_coefficients.py
@@ -3,20 +3,6 @@ import numpy as np
 from hypothesis import given, strategies as st
 from CircuitCalculator.SignalProcessing.periodic_functions import fourier_series_mapping, AbstractHarmonicCoefficients
 
-@given(st.floats(min_value=0, allow_subnormal=False), st.floats(min_value=0, max_value=2*np.pi, allow_subnormal=False), st.integers(max_value=-1))
-def test_amplitude_method_raises_error_when_passing_negative_index(A, phi, index) -> None:
-    for harmonic_implementator in fourier_series_mapping.values():
-        harmonic_implementation = harmonic_implementator(A, phi, 0)
-        with pytest.raises(ValueError):
-            harmonic_implementation.amplitude(index)
-
-@given(st.floats(min_value=0, allow_subnormal=False), st.floats(min_value=0, max_value=2*np.pi, allow_subnormal=False), st.integers(max_value=-1))
-def test_phase_method_raises_error_when_passing_negative_index(A, phi, index) -> None:
-    for harmonic_implementator in fourier_series_mapping.values():
-        harmonic_implementation = harmonic_implementator(A, phi, 0)
-        with pytest.raises(ValueError):
-            harmonic_implementation.phase(index)
-
 @given(st.floats(min_value=0, allow_subnormal=False), st.floats(min_value=0, max_value=2*np.pi, allow_subnormal=False))
 def test_a_coefficient_is_correct(A, phi) -> None:
     class HarmonicImplementation(AbstractHarmonicCoefficients):
@@ -35,7 +21,7 @@ def test_b_coefficient_is_correct(A, phi) -> None:
         def _phase_coefficient(self, _):
             return self.phase0
     hi = HarmonicImplementation(A, phi)
-    assert hi.b(0) == A*np.sin(phi)
+    assert hi.b(0) == -A*np.sin(phi)
 
 @given(st.floats(min_value=0, allow_subnormal=False, allow_infinity=False), st.floats(min_value=0, max_value=2*np.pi, allow_subnormal=False))
 def test_complex_part_is_correct(A, phi) -> None:
@@ -45,4 +31,4 @@ def test_complex_part_is_correct(A, phi) -> None:
         def _phase_coefficient(self, _):
             return self.phase0
     hi = HarmonicImplementation(A, phi)
-    assert hi.c(0) == A/2*np.exp(-1j*phi)
+    assert hi.c(0) == A/2*np.exp(1j*phi)
