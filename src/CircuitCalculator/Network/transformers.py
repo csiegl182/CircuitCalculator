0cac20cac536e5b248f8339c4ee5edc85fdd28f1
Christian Siegl <christian.siegl@gmail.com>
2025-04-05 08:27:00 +0200

    fix issues with open circuit impedance calculation

--- src/CircuitCalculator/Network/transformers.py
+++ src/CircuitCalculator/Network/transformers.py
@@ -5,8 +5,27 @@ def switch_ground_node(network: Network, new_ground: str) -> Network:
     return Network(network.branches, new_ground)
 
 def remove_element(network: Network, element: str) -> Network:
-    branches = list(network.branches)
-    branches.remove(network[element])
+    branch_to_remove = network[element]
+    if branch_to_remove.node1 == network.node_zero_label and len(network.branches_connected_to(branch_to_remove.node1)) == 1:
+       network = rename_node(network, branch_to_remove.node2, network.node_zero_label) 
+    if branch_to_remove.node2 == network.node_zero_label and len(network.branches_connected_to(branch_to_remove.node2)) == 1:
+       network = rename_node(network, branch_to_remove.node1, network.node_zero_label) 
+    branches = [b for b in network.branches if b.element.name != element]
+    return Network(branches, node_zero_label=network.node_zero_label)
+    
+def remove_elements(network: Network, elements: list[str]) -> Network:
+    for element in elements:
+        network = remove_element(network, element)
+    return network
+
+def rename_node(network: Network, old_node: str, new_node: str) -> Network:
+    def rename(branch: Branch) -> Branch:
+        if branch.node1 == old_node:
+            branch = Branch(new_node, branch.node2, branch.element)
+        if branch.node2 == old_node:
+            branch = Branch(branch.node1, new_node, branch.element)
+        return branch
+    branches = [rename(b) for b in network.branches]
     return Network(branches, node_zero_label=network.node_zero_label)
 
 def remove_open_circuit_elements(network: Network) -> Network:
--- tests/Network/transformers/test_remove_element.py
+++ tests/Network/transformers/test_remove_element.py
@@ -36,3 +36,18 @@ def test_remove_network_element_removes_unknown_element() -> None:
     )
     with pytest.raises(KeyError):
         remove_element(network, 'Usx')
+
+def test_remove_element_keeps_zero_node_label() -> None:
+    r1 = resistor('R1', R=1)
+    r2 = resistor('R2', R=2)
+    network = Network(
+        branches=[
+            Branch('2', '1', r1),
+            Branch('1', '0', r2)
+        ]
+    )
+    new_network = remove_element(network, 'R2')
+    assert new_network.node_zero_label == '0'
+    assert new_network['R1'].node1 == '2'
+    assert new_network['R1'].node2 == '0'
+    
