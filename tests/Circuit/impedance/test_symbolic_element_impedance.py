cf76234d3313ef7ab8b8f544f3ecdf881a39885f
Christian Siegl <christian.siegl@gmail.com>
2025-04-15 06:31:45 +0800

    imporve impedance tests for symbolic bahaviour

--- tests/Circuit/impedance/test_symbolic_element_impedance.py
+++ tests/Circuit/impedance/test_symbolic_element_impedance.py
@@ -4,47 +4,46 @@ import CircuitCalculator.Circuit.Components.symbolic_components as ccp
 import sympy as sp
 
 def test_element_impedance_of_simple_network_can_be_calculated() -> None:
-    R = '1'
+    R2 = sp.symbols('R2', real=True, positive=True)
     circuit = Circuit([
-        ccp.resistor(R=R, id='R1', nodes=('1', '0')),
-        ccp.resistor(R=R, id='R2', nodes=('1', '0'))
+        ccp.resistor(id='R1', nodes=('1', '0')),
+        ccp.resistor(id='R2', nodes=('1', '0'))
     ])
-    assert symbolic_element_impedance(circuit, 'R1') == sp.sympify(R)
+    assert symbolic_element_impedance(circuit, 'R1') == R2
 
 def test_element_impedance_of_real_voltage_source() -> None:
-    R1, R2 = '1', '2'
+    R1 = sp.symbols('R1', real=True, positive=True)
     circuit = Circuit([
         ccp.voltage_source(id='V', nodes=('1', '0')),
-        ccp.resistor(R=R1, id='R1', nodes=('1', '2')),
-        ccp.resistor(R=R2, id='R2', nodes=('2', '0')),
+        ccp.resistor(id='R1', nodes=('1', '2')),
+        ccp.resistor(id='R2', nodes=('2', '0')),
         ccp.ground(nodes=('0',))
     ])
-    assert symbolic_element_impedance(circuit, 'R2') == sp.sympify(R1)
+    assert symbolic_element_impedance(circuit, 'R2') == R1
 
 def test_element_impedance_of_linear_voltage_source() -> None:
-    R1, R2 = '1', '2'
     circuit = Circuit([
-        ccp.voltage_source(R=R1, id='V', nodes=('1', '0')),
-        ccp.resistor(R=R2, id='R2', nodes=('1', '0')),
+        ccp.voltage_source(id='V', nodes=('1', '0')),
+        ccp.resistor(id='R2', nodes=('1', '0')),
         ccp.ground(nodes=('0',))
     ])
-    assert symbolic_element_impedance(circuit, 'R2') == sp.sympify(R1)
+    assert symbolic_element_impedance(circuit, 'R2') == 0
 
 def test_element_impedance_of_real_current_source() -> None:
-    R1, R2 = '1', '2'
+    R1 = sp.symbols('R1', real=True, positive=True)
     circuit = Circuit([
         ccp.current_source(id='I', nodes=('1', '0')),
-        ccp.resistor(R=R1, id='R1', nodes=('1', '0')),
-        ccp.resistor(R=R2, id='R2', nodes=('1', '0')),
+        ccp.resistor(id='R1', nodes=('1', '0')),
+        ccp.resistor(id='R2', nodes=('1', '0')),
         ccp.ground(nodes=('0',))
     ])
-    assert symbolic_element_impedance(circuit, 'R2') == sp.sympify(R1)
+    assert symbolic_element_impedance(circuit, 'R2') == R1
 
 def test_element_impedance_of_linear_current_source() -> None:
-    R1, R2 = '1', '2'
+    G1 = sp.sympify('G1')
     circuit = Circuit([
-        ccp.current_source(G=f'1/{R1}', id='I', nodes=('1', '0')),
-        ccp.resistor(R=R2, id='R2', nodes=('1', '0')),
+        ccp.current_source(G='G1', id='I', nodes=('1', '0')),
+        ccp.resistor(id='R2', nodes=('1', '0')),
         ccp.ground(nodes=('0',))
     ])
-    assert symbolic_element_impedance(circuit, 'R2') == sp.sympify(R1)
+    assert symbolic_element_impedance(circuit, 'R2') == 1/G1
--- tests/Circuit/impedance/test_symbolic_open_circuit_impedance.py
+++ tests/Circuit/impedance/test_symbolic_open_circuit_impedance.py
@@ -5,54 +5,53 @@ import sympy as sp
 from numpy.testing import assert_almost_equal
 
 def test_impedance_of_simple_network_can_be_calculated() -> None:
-    R1, R2 = '2', '3'
+    R1, R2 = sp.symbols('R1 R2', real=True, positive=True)
     circuit = Circuit([
-        cp.resistor(R=R1, id='R1', nodes=('1', '0')),
-        cp.resistor(R=R2, id='R2', nodes=('1', '0'))
+        cp.resistor(id='R1', nodes=('1', '0')),
+        cp.resistor(id='R2', nodes=('1', '0'))
     ])
-    assert symbolic_open_circuit_dc_resistance(circuit, '1', '0') == sp.sympify(f'1/(1/{R1} + 1/{R2})')
+    assert symbolic_open_circuit_dc_resistance(circuit, '1', '0') == 1/(1/R1+1/R2)
 
 def test_impedance_of_inductance_is_zero() -> None:
-    L = '100'
     circuit = Circuit([
-        cp.inductance(L=L, id='L', nodes=('1', '0')),
+        cp.inductance(id='L', nodes=('1', '0')),
         cp.ground(nodes=('0',))
     ])
     assert symbolic_open_circuit_dc_resistance(circuit, '1', '0') == 0
 
 def test_impedance_of_resistor_with_following_short_circuit() -> None:
-    R = '100'
+    R = sp.symbols('R', real=True, positive=True)
     circuit = Circuit([
-        cp.resistor(R=R, id='R', nodes=('2', '0')),
+        cp.resistor(id='R', nodes=('2', '0')),
         cp.short_circuit(id='X', nodes=('1', '2')),
     ])
-    assert symbolic_open_circuit_dc_resistance(circuit, '1', '0') == sp.sympify(R)
+    assert symbolic_open_circuit_dc_resistance(circuit, '1', '0').nsimplify() == R
 
 def test_impedance_of_resistor_with_voltage_source() -> None:
-    R = '100'
+    R1, R2 = sp.symbols('R1 R2', real=True, positive=True)
     cicuit = Circuit([
         cp.voltage_source(id='V', nodes=('1', '0')),
-        cp.resistor(R=R, id='R1', nodes=('1', '2')),
-        cp.resistor(R=R, id='R2', nodes=('2', '0')),
+        cp.resistor(id='R1', nodes=('1', '2')),
+        cp.resistor(id='R2', nodes=('2', '0')),
         cp.ground(nodes=('0',)),
     ])
-    assert_almost_equal(float(symbolic_open_circuit_dc_resistance(cicuit, '2', '0')), float(sp.sympify(f'{R}/2')), decimal=5)
+    assert symbolic_open_circuit_dc_resistance(cicuit, '2', '0').simplify().nsimplify() == R1*R2/(R1+R2)
 
 def test_impedance_of_resistor_with_current_source() -> None:
-    R = '100'
+    R = sp.symbols('R', real=True, positive=True)
     cicuit = Circuit([
         cp.current_source(id='I', nodes=('1', '0')),
-        cp.resistor(R=R, id='R', nodes=('1', '0')),
+        cp.resistor(id='R', nodes=('1', '0')),
         cp.ground(nodes=('0',)),
     ])
-    assert symbolic_open_circuit_dc_resistance(cicuit, '1', '0') == sp.sympify(R)
+    assert symbolic_open_circuit_dc_resistance(cicuit, '1', '0') == R
 
 def test_impedance_of_resistor_and_capacitor() -> None:
-    R, C = '100', '200'
+    R = sp.symbols('R', real=True, positive=True)
     cicuit = Circuit([
         cp.voltage_source(id='V', nodes=('1', '0')),
-        cp.resistor(R=R, id='R', nodes=('1', '2')),
-        cp.capacitor(C=C, id='C', nodes=('2', '0')),
+        cp.resistor(id='R', nodes=('1', '2')),
+        cp.capacitor(id='C', nodes=('2', '0')),
         cp.ground(nodes=('0',)),
     ])
-    assert symbolic_open_circuit_dc_resistance(cicuit, '2', '0') == sp.sympify(R)
+    assert symbolic_open_circuit_dc_resistance(cicuit, '2', '0').nsimplify() == R
